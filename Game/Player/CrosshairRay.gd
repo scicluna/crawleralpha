bf5eadb53edb29bded46780e788544a9fa192a36
John Scicluna <john_scicluna@mied.uscourts.gov>
2024-05-30 14:40:53 -0400

    added floating damage text and a color changing crosshair

---
+++ Game/Player/CrosshairRay.gd
@@ -0,0 +1,24 @@
+extends RayCast3D
+signal mob_detected(is_detected: bool)
+
+@onready var cross_hair := $"../CanvasLayer/Control/Crosshair"
+const DEFAULT_COLOR: Color = Color(1,1,1,1)
+const HOVER_COLOR: Color = "RED"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta: float) -> void:
+	if is_colliding():
+		var collider = get_collider()
+		if collider is Mob:
+			cross_hair.modulate = HOVER_COLOR
+		else:
+			cross_hair.modulate = DEFAULT_COLOR
+		
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
